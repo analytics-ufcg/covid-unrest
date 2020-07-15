495b9d44d36d0ce2d5d34852122294301d77f73f
Marcus Carvalho <marcuswac@gmail.com>
2020-07-14 22:17:53 -0300

    replacing IBGE city code and fixing col names

--- src/extract_covid_data.R
+++ src/extract_covid_data.R
@@ -2,10 +2,15 @@ library(dplyr, quietly = TRUE, warn.conflicts = FALSE)
 library(here, quietly = TRUE)
 library(readr, quietly = TRUE)
 library(readxl, quietly = TRUE)
+library(tidylog, warn.conflicts = FALSE)
 
 # Download xslx file at https://covid.saude.gov.br/
 
 load_covid_data_ms <- function(file_xlsx, level = NULL) {
+  city_codes_ibge <- read_csv(here("data", "ready", "city-codes-ibge.csv"),
+                              col_types = cols()) %>%
+    select(municipio_code, municipio_code6)
+  
   covid_data <- read_xlsx(file_xlsx, guess = 10^4) %>%
     mutate(
       data = as.Date(data),
@@ -13,7 +18,14 @@ load_covid_data_ms <- function(file_xlsx, level = NULL) {
       incidencia100k = round(10^5 * casosAcumulado / populacaoTCU2019, 2),
       mortalidade100k = round(10^5 * obitosAcumulado / populacaoTCU2019, 2)
     ) %>%
-    arrange(estado, municipio, data)
+    arrange(estado, municipio, data) %>%
+    left_join(city_codes_ibge, by = c("codmun" = "municipio_code6")) %>%
+    relocate(municipio_code, .before = codmun) %>%
+    select(-codmun) %>%
+    janitor::clean_names() %>%
+    rename(recuperados_novos = recuperadosnovos,
+           cod_uf = coduf,
+           cod_municipio = municipio_code)
   
   if (!is.null(level)) {
     covid_data <- switch(level,
@@ -28,7 +40,7 @@ load_covid_data_ms <- function(file_xlsx, level = NULL) {
 
 extract_covid_data_by_country <- function(covid_data) {
   by_country <- covid_data %>%
-    filter(!is.na(regiao), is.na(codmun), is.na(estado)) %>%
+    filter(!is.na(regiao), is.na(cod_municipio), is.na(estado)) %>%
     select_if(~any(!is.na(.)))
   
   return(by_country)
@@ -36,7 +48,7 @@ extract_covid_data_by_country <- function(covid_data) {
 
 extract_covid_data_by_state <- function(covid_data) {
   by_state <- covid_data %>%
-    filter(is.na(codmun), !is.na(estado)) %>%
+    filter(is.na(cod_municipio), !is.na(estado)) %>%
     select_if(~any(!is.na(.)))
   
   return(by_state)
@@ -44,7 +56,7 @@ extract_covid_data_by_state <- function(covid_data) {
 
 extract_covid_data_by_city <- function(covid_data) {
   by_city <- covid_data %>%
-    filter(!is.na(codmun), !is.na(estado)) %>%
+    filter(!is.na(cod_municipio), !is.na(estado)) %>%
     select_if(~any(!is.na(.)))
   
   return(by_city)
@@ -53,18 +65,24 @@ extract_covid_data_by_city <- function(covid_data) {
 main <- function(argv) {
   if (length(argv) < 1) {
     stop(paste("Usage: Rscript extract_covid_data.R <covid-data-file.xlsx>",
-               "[aggregation level (city, state, country)] [output csv file]"))
+               "[aggregation level (all, city, state, country)] [output dir]"))
   }
   xlsx_file <- argv[1]
-  agg_level <- ifelse(length(argv) >= 2, tolower(argv[2]), "state")
-  output_file <- ifelse(
-    length(argv) >= 3, tolower(argv[3]),
-    here("data", "ready", paste0("covid-data-br-", agg_level, ".csv"))
-  )
-
-  covid_data <- load_covid_data_ms(xlsx_file, agg_level)
-  write_csv(covid_data, output_file, na = "")
-  message("Data written to ", output_file)
+  agg_level <- ifelse(length(argv) >= 2, tolower(argv[2]), "all")
+  output_dir <- ifelse(length(argv) >= 3, argv[3], here("data", "ready"))
+  
+  all_agg_levels <- c("city", "state", "country")
+  
+  if (agg_level == "all") {
+    agg_level <- all_agg_levels
+  }
+  
+  for (agg in agg_level) {
+    output_file <- file.path(output_dir, paste0("covid-data-br-", agg, ".csv"))
+    covid_data <- load_covid_data_ms(xlsx_file, agg)
+    write_csv(covid_data, output_file, na = "")
+    message("Data written to ", output_file)
+  }
 }
 
 if (!interactive()) {
